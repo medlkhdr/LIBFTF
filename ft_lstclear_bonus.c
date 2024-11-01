4c44e109b51024fa61657739ec6a71603a648718
Mohamed Lakhdar <mlakhdar@e2r2p2.1337.ma>
2024-11-01 15:15:20 +0100

    OWNED BY HUNTER

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlakhdar <mlakhdar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 00:20:27 by mlakhdar          #+#    #+#             */
+/*   Updated: 2024/10/30 05:00:35 by mlakhdar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*current;
+	t_list	*next_node;
+
+	if (!lst || !(*lst))
+	{
+		return ;
+	}
+	current = *lst;
+	while (current)
+	{
+		next_node = current->next;
+		ft_lstdelone(current, del);
+		current = next_node;
+	}
+	*lst = NULL;
+}
